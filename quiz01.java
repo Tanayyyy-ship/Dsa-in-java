de5eea0926ed50eb1abc585fb67cd7b116c66e6a
Tanayyy <pandeytanay926@gmail.com>
2026-06-11 22:24:07 +0530

    Add files via upload

---
+++ quiz01.java
@@ -0,0 +1,19 @@
+public class quiz01 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        int diff = a - b;
+        System.out.println("\n" + diff);
+        
+        int mul = a * b ;
+        System.out.println("\n" + mul);
+
+        int ans  = (a*b)/(a-b);
+        System.out.println("\n" + ans);
+
+    
+
+    
+    }
+}
