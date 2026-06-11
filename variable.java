349a8de4514f0ef9db8163ea96b39b949d37e5fa
Tanayyy <pandeytanay926@gmail.com>
2026-06-11 22:23:31 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,18 @@
+public class variable {
+    public static void main(String[] args) {
+        String name = "Tanay Pandey";
+        int age = 19;
+        double price = 25.25;
+        int a = 25;
+        int b = 10;
+
+        b = 20;
+        name = "shaktiman";
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(price);
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
