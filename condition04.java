2ca30d32c783d27d1f28e24384880949ec9345b1
Tanayyy <pandeytanay926@gmail.com>
2026-06-13 14:45:02 +0530

    Add files via upload

---
+++ condition04.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class condition04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+
+        if (a>b) {
+            System.out.println("a is greater then b") ;
+        } else {
+            System.out.println("a is smaller then b") ;
+
+
+            sc.close();
+        }
+    }
+
+}
