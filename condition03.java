fa5c79d3c9f550dfdb88a5cd8f0af90ec2b17e62
Tanayyy <pandeytanay926@gmail.com>
2026-06-13 14:44:46 +0530

    Add files via upload

---
+++ condition03.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class condition03 {
+    public static void main(String [] args){
+        Scanner sc = new Scanner (System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a==b){
+            System.out.println("no are equal");
+
+        } else {
+            System.out.println("no  are not eqal");
+
+    
+        
+
+            sc.close();
+        }
+
+
+
+
+
+    }
+
+}
