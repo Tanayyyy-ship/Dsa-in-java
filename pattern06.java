1ca5c2631d521f1a023d16173d07cff8b66f4237
Tanayyy <pandeytanay926@gmail.com>
2026-06-20 23:26:25 +0530

    Add files via upload

---
+++ pattern06.java
@@ -0,0 +1,12 @@
+public class pattern06 {
+    public static void main(String[] agrs){
+        int n = 5;
+        for(int i = 1; i<=n; i++){
+            for(int j =1; j<=n-i+1; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+    
+}
